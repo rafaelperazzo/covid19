2b5ff234da24aab6c5ee64e0a27e23bba11b643e
Rafael Perazzo <rafaelperazzo@gmail.com>
2020-05-15 14:08:09 -0300

    Atualizado 2020-05-15 14:08:09

--- covid.py
+++ covid.py
@@ -35,14 +35,14 @@ profile.set_preference("browser.download.dir", CSV_DIR)
 profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;text/csv")
 
 os.chdir(CSV_DIR)
-os.system('rm -f DT_PAINEL*.xlsx')
+os.system('rm -f HIST_PAINEL*.xlsx')
 
 #abrindo o navegador
 print("Abrindo o navegador...")
 driver = webdriver.Firefox(options=options,firefox_profile=profile)
 
 #BAIXANDO DO MINISTERIO DA SAUDE
-os.system('rm -f DT_PAINEL*.xlsx')
+os.system('rm -f HIST_PAINEL*.xlsx')
 driver.get(url)
 sleep(3)
 botao = driver.find_element_by_css_selector("div.no-shadow:nth-child(2) > ion-button:nth-child(1)")
@@ -52,7 +52,7 @@ sleep(5)
 driver.quit()
 print("Arquivo salvo com sucesso! Fechando navegador!")
 #Renomeando o arquivo
-os.system('mv DT_PAINEL*.xlsx cidades.brasil.hoje.xlsx')
+os.system('mv HIST_PAINEL*.xlsx cidades.brasil.hoje.xlsx')
 print("Convertendo arquivo...")
 df = pd.read_excel(CSV_DIR + 'cidades.brasil.hoje.xlsx')
 df.to_csv(CSV_DIR + 'cidades.brasil.hoje.csv',index=False,header=True)
