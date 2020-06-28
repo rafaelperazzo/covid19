3617c689ab63ba20d6206bd1679840f58082b512
Rafael Perazzo <rafaelperazzo@gmail.com>
2020-06-28 19:56:01 -0300

    Atualizado 2020-06-28 19:56:01

--- covid_scrapy.py
+++ covid_scrapy.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import TimeoutException
 from time import sleep
 import time
 from datetime import date
@@ -16,6 +17,18 @@ from datetime import date
 CSV_DIR = '/dados/www/html/covid_csv/spyder/'
 
 url = 'https://indicadores.integrasus.saude.ce.gov.br/indicadores/indicadores-coronavirus/coronavirus-ceara'
+
+def aguardaAparecerCarregando(driver,delay):
+    try:
+        myElem = WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.CLASS_NAME, 'app-loading')))
+    except TimeoutException:
+        print ("FALHOU! NÃO apareceu o carregando!")
+
+def aguardaDesaparecerCarregando(driver,delay):
+    try:
+        myElem = WebDriverWait(driver, delay).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'app-loading')))
+    except TimeoutException:
+        print ("FALHOU! NÃO sumiu o carregando")
 
 def getHTML(diaInicial=1,mes='03',headless=False,hoje=True,linhas=[],colunas=[],maxDia=31,anterior=True):
 
@@ -27,49 +40,95 @@ def getHTML(diaInicial=1,mes='03',headless=False,hoje=True,linhas=[],colunas=[],
     driver = webdriver.Firefox(options=options)
     #BAIXANDO DA SECRETARIA DA SAUDE/CE
     driver.get(url)
-    sleep(20)
+    delay = 120
+    aguardaAparecerCarregando(driver,delay)
+    aguardaDesaparecerCarregando(driver,delay)
     webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-    sleep(30)
+    delay = 120
     if (hoje): #Baixa apenas a tabela do dia
+        print("Baixando: " + data_hoje)
+        try:
+            myElem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CLASS_NAME, 'mat-select-arrow-wrapper')))
+            myElem = WebDriverWait(driver, delay).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'app-loading')))
+            
+        except TimeoutException:
+            print ("FALHOU! NÃO CARREGOU ELEMENTO!")
         botao = driver.find_element_by_class_name("mat-select-arrow-wrapper")
         botao.click()
-        sleep(20)
+        try:
+            myElem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CLASS_NAME, 'mat-option-text')))
+            
+        except TimeoutException:
+            print ("FALHOU! NÃO CARREGOU ELEMENTO!")
         botao = driver.find_element_by_class_name("mat-option-text")
         botao.click()
-        sleep(20)
+        aguardaAparecerCarregando(driver,delay)
+        aguardaDesaparecerCarregando(driver,delay)
+        '''
+        try:
+            myElem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CLASS_NAME, 'mat-option-text')))
+            
+        except TimeoutException:
+            print ("FALHOU! NÃO CARREGOU ELEMENTO!")
+        '''
         html = driver.page_source
         with open(CSV_DIR + 'sec-ce-' + data_hoje +'.html', 'w') as arquivo:
             arquivo.write(html)
             arquivo.close()
+            nomearquivo = CSV_DIR + 'sec-ce-' + data_hoje +'.html'
+            print(nomearquivo + " gravado com sucesso...")
     else: #baixa a tabela de um intervalo de dias
         dia = diaInicial
         clicouNoMesAnterior = anterior
+        try:
+            myElem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CLASS_NAME, 'mat-select-arrow-wrapper')))
+            myElem = WebDriverWait(driver, delay).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'app-loading')))
+            
+        except TimeoutException:
+            print ("FALHOU! NÃO CARREGOU ELEMENTO!")
         botao = driver.find_element_by_class_name("mat-select-arrow-wrapper")
         botao.click()
-        sleep(20)
+        try:
+            myElem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CLASS_NAME, 'mat-option-text')))
+            myElem = WebDriverWait(driver, delay).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'app-loading')))
+            
+        except TimeoutException:
+            print ("FALHOU! NÃO CARREGOU ELEMENTO: mat-option-text 1")
         botao = driver.find_element_by_class_name("mat-option-text")
         botao.click()
-        sleep(20)
+        
+        aguardaAparecerCarregando(driver,delay)
+        aguardaDesaparecerCarregando(driver,delay)
         webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-        sleep(20)
+        
         for i in linhas:
             for j in colunas:
                 #Clica no calendario
+                try:
+                    myElem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.mat-datepicker-toggle-default-icon')))
+                except TimeoutException:
+                    print ("FALHOU! NÃO CARREGOU ELEMENTO!")
                 driver.find_element_by_css_selector(".mat-datepicker-toggle-default-icon").click()
-                sleep(20)
                 #Clica no mês anterior
                 if (not clicouNoMesAnterior):
+                    try:
+                        myElem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.mat-calendar-previous-button')))
+                        
+                    except TimeoutException:
+                        print ("FALHOU! NÃO CARREGOU ELEMENTO!")
                     driver.find_element_by_css_selector(".mat-calendar-previous-button").click()
                     clicouNoMesAnterior = True
-                    sleep(20)
-
+                    
                 try:
                     if (dia>maxDia):
                         driver.quit()
                         return
                     #Clica no dia
+                    print("Aguardando o dia..." + mes + "-" + '{:02d}'.format(dia))
+                    
                     driver.find_element_by_css_selector(".mat-calendar-body > tr:nth-child(" + str(i) + ") > td:nth-child(" + str(j) + ") > div:nth-child(1)").click()
-                    sleep(20)
+                    aguardaAparecerCarregando(driver,delay)
+                    aguardaDesaparecerCarregando(driver,delay)
                     #Monta o nome do arquivo
                     nomearquivo = CSV_DIR + "sec-ce-2020-" + mes + "-" + '{:02d}'.format(dia) + ".html"
                     dia = dia + 1
@@ -104,9 +163,9 @@ getHTML(headless=True,hoje=True)
 #getHTML(diaInicial=1,mes="05",headless=True,hoje=False,linhas=range(1,2,1),colunas=range(2,4,1),maxDia=2,anterior=True)
 
 #print("Baixando mês de maio de 3 a 31")
-#getHTML(diaInicial=3,mes="05",headless=True,hoje=False,linhas=range(2,7,1),colunas=range(1,8,1),maxDia=31,anterior=False)
+#getHTML(diaInicial=3,mes="05",headless=False,hoje=False,linhas=range(2,7,1),colunas=range(1,8,1),maxDia=31,anterior=False)
 
 #print("Baixando mês de junho de 1 até 6")
-#getHTML(diaInicial=1,mes="06",headless=True,hoje=False,linhas=range(2,7,1),colunas=range(2,8,1),maxDia=6,anterior=True)
+#getHTML(diaInicial=1,mes="06",headless=False,hoje=False,linhas=range(2,7,1),colunas=range(2,8,1),maxDia=6,anterior=True)
 print("Baixando mês de junho de 7 até 30")
-getHTML(diaInicial=7,mes="06",headless=True,hoje=False,linhas=range(3,7,1),colunas=range(2,8,1),maxDia=dia_hoje-1,anterior=True)
+getHTML(diaInicial=7,mes="06",headless=True,hoje=False,linhas=range(3,7,1),colunas=range(1,8,1),maxDia=dia_hoje-1,anterior=True)
